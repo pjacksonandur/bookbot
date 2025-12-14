d66431fd21c9d345069b8d77b9ebefae5eaaa410
pjackson <pjackson@anduril.com>
2025-12-14 11:25:09 -0800

    new file:   .gitignore 	new file:   main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+
+def get_book_text():
+    filePath = '/Users/pjackson/Documents/Development/bootDevProjects/bookbot/books/frankenstein.txt'
+    return filePath  
+def main():
+    with open(get_book_text()) as f:
+        fileContents = f.read()
+        print(fileContents)
+def acceptText(main()):
+    main.split()
+    
+    
+main()
+
+
+
+
+
